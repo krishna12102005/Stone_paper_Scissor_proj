8c75fe4c6c46125cc6bc13b93c20340747446ca8
krishnakalaiselvan12@gmail.com <krishnakalaiselvan12@gmail.com>
2025-04-12 17:04:42 +0530

    this project is completed

--- project.py
+++ project.py
@@ -1,1 +1,44 @@
-print("hello world")
+import random
+
+choices = ["Rock","Paper","Scissors"]
+computer_guess = random.choice(choices)
+
+
+while True:
+    user_input = input("Rock, Paper or Scissors (r/p/s): ").lower()
+
+    if user_input == "r":
+        print("You chose Rock")
+        print("Computer chose", computer_guess)
+
+        if computer_guess == "Paper":
+            print("You lose")
+        elif computer_guess == "Scissors":
+            print("You win")
+        else:
+            print("Both are the same")
+
+    elif user_input == "p":  # <- Correct indentation here
+        print("You chose Paper")
+        print("Computer chose", computer_guess)
+
+        if computer_guess == "Scissors":
+            print("You lose")
+        elif computer_guess == "Rock":
+            print("You win")
+        else:
+            print("Both are the same")
+
+    elif user_input == "s":
+        print("You chose scissors")
+        print("Computer chose", computer_guess)
+
+        if computer_guess == "Rock":
+            print("You lose ")
+        elif computer_guess == "Paper":
+            print("You win")
+        else:
+            print("Both are the same")
+
+    else:
+        print("Invalid Input!")
